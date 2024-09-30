69d358d885cbdd1a1114ee64af905adb540d5eaf
Arthur Kozhemyakin <trable228@icloud.com>
2024-09-30 11:18:54 +0300

    update .gitignore

--- app/src/main/java/com/example/dentaltechnicianhandbook/ui/viewmodel/DataModel.kt
+++ app/src/main/java/com/example/dentaltechnicianhandbook/ui/viewmodel/DataModel.kt
@@ -4,9 +4,11 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class DataModel : ViewModel() {
+
     val kind: MutableLiveData<String> by lazy {
         MutableLiveData<String>()
     }
+
     val steps: MutableLiveData<Int> by lazy {
         MutableLiveData<Int>()
     }
